94987ae1a056725f07fec6b71138a932074cb308
Albert Luta <github.7ntgj@simplelogin.co>
2022-07-23 00:55:39 +0300

    chore(deps): upgrade vite to v3

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   '*.{json,yml,yaml,md}': ['prettier --write'],
   '*.css': ['stylelint --fix', 'prettier --write'],
   '*.{ts,tsx}': ['eslint --fix', () => 'tsc --noEmit', 'prettier --write'],
